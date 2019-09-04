eec81c883ce0b34bd1cee07cc761fb965f2bdff6
ryleyrandall22 <33163774+ryleyrandall22@users.noreply.github.com>
2019-09-04 15:28:10 -0600

    Updated Firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const config = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
+  apiKey: "AIzaSyAQm18gk559JFRNcvSNfWfgAskYCqlrqzg",
+  authDomain: "authentication2-ryley.firebaseapp.com",
+  databaseURL: "https://authentication2-ryley.firebaseio.com",
+  projectId: "authentication2-ryley",
   storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  messagingSenderId: "424246983876",
+  appId: "1:424246983876:web:0c1cd11f10a853672a9d91"
 };
 
 firebase.initializeApp(config);
